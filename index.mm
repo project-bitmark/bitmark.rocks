3a7e214b2c5339e54780ed2e7d77cc1c625390f0
Melvin Carvalho <melvincarvalho@gmail.com>
2021-04-14 10:12:10 +0200

    first web map

---
+++ index.mm
@@ -0,0 +1,94 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="bitmark.rocks" FOLDED="false" ID="ID_9134036" CREATED="1618386417863" MODIFIED="1618386814419" LINK="../../../../www.localhost/mm/2021/04/14/index.mm">
+<icon BUILTIN="bookmark"/>
+<edge STYLE="bezier" COLOR="#007c00" WIDTH="thin" DASH="SOLID"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1426" RULE="ON_BRANCH_CREATION"/>
+<font SIZE="18"/>
+<node TEXT="website" POSITION="left" ID="ID_1060112625" CREATED="1618386591344" MODIFIED="1618386933481">
+<icon BUILTIN="internet"/>
+<edge COLOR="#ff00ff"/>
+<node TEXT="bitmark.co" ID="ID_909182614" CREATED="1618386598162" MODIFIED="1618386981598" LINK="https://www.bitmark.io/"/>
+</node>
+<node TEXT="explorer" POSITION="left" ID="ID_890451887" CREATED="1618386602019" MODIFIED="1618386922491">
+<icon BUILTIN="emoji-1F4C1"/>
+<edge COLOR="#00ffff"/>
+<node TEXT="chainz" ID="ID_1364530620" CREATED="1618386616985" MODIFIED="1618386966992" LINK="https://chainz.cryptoid.info/marks/"/>
+<node TEXT="bitmark.co" ID="ID_281551312" CREATED="1618386805575" MODIFIED="1618386963218" LINK="https://explorer.bitmark.co/"/>
+<node TEXT="plots" ID="ID_387892306" CREATED="1618386792215" MODIFIED="1618386793755">
+<node TEXT="plots" ID="ID_696116134" CREATED="1618386794806" MODIFIED="1618386973287" LINK="https://explorer.bitmark.co/plots.week/"/>
+</node>
+</node>
+<node TEXT="source" POSITION="right" ID="ID_1630806653" CREATED="1618386621727" MODIFIED="1618386825250">
+<icon BUILTIN="emoji-1F528"/>
+<edge COLOR="#7c0000"/>
+<node TEXT="github" ID="ID_1135958046" CREATED="1618386670423" MODIFIED="1618386990767" LINK="https://github.com/project-bitmark/bitmark"/>
+</node>
+<node TEXT="exchange" POSITION="right" ID="ID_1068324254" CREATED="1618386682800" MODIFIED="1618386849237">
+<icon BUILTIN="emoji-1F3B0"/>
+<edge COLOR="#00007c"/>
+<node TEXT="freiexchange" ID="ID_1824557321" CREATED="1618386707851" MODIFIED="1618386996415" LINK="https://freiexchange.com/market/BTM/BTC"/>
+</node>
+</node>
+</map>
